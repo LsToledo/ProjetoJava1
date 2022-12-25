45dc532b49c47d66cf1c8341c50198ec63ec125b
LsToledo <luissoaresof.la@gmail.com>
2022-12-25 18:56:18 -0300

    comit1

--- PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
+++ PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
@@ -3,7 +3,7 @@ package curso.java.executavel;
 public class PrimeiraClasseJava {
 
 	
-/*comentario  gg*/
+/*comentario  gggg*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helow Word!");
