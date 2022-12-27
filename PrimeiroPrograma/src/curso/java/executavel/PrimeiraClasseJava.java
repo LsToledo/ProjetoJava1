4f0dd1d00ac9f03e8c2ae1bf1c3cfe4f2fe3e243
LsToledo <luissoaresof.la@gmail.com>
2022-12-27 05:49:08 -0300

    organizando

--- PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
+++ PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
@@ -10,15 +10,14 @@ public class PrimeiraClasseJava {
 	public int getIdade() {
 		return idade;
 	}
-	public Data getDataNacimento() {
+
+    public Data getDataNacimento() {
 		return dataNacimento;
 	}
 	public void setDataNacimento(Data dataNacimento) {
 		this.dataNacimento = dataNacimento;
 	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
+
 	public String getNome() {
 		return nome;
 	}
