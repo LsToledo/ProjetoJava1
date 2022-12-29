20f37525b134d48652f6f61a4ad8ae5b3ecd1f18
LsToledo <luissoaresof.la@gmail.com>
2022-12-29 16:14:41 -0300

    Crtl+shft+f

--- PrimeiroPrograma/src/curso/java/executavel/GERAL.java
+++ PrimeiroPrograma/src/curso/java/executavel/GERAL.java
@@ -9,14 +9,13 @@ public class GERAL {
 		double nota4 = 80;
 		double notaMedia = ((nota1 + nota2 + nota3 + nota4) / 4);
 		String name = "Luis";
-/*
-		if (notaMedia >= 70) {
-			System.out.println("Aprovado :" + notaMedia + " " + nome);
-		} else {
-			System.out.println("Reprovado:" + notaMedia + " " + nome);
-		} */
-		String saidaResultado = notaMedia >= 70 ? "Aprovado" : (notaMedia >=40 && notaMedia<=60)? "Recuperaçao" : "Reprovado";
-		System.out.println(saidaResultado + " " + notaMedia +" "+ "Aluno: "+" " +name);
+		/*
+		 * if (notaMedia >= 70) { System.out.println("Aprovado :" + notaMedia + " " +
+		 * nome); } else { System.out.println("Reprovado:" + notaMedia + " " + nome); }
+		 */
+		String saidaResultado = notaMedia >= 70 ? "Aprovado"
+				: (notaMedia >= 40 && notaMedia <= 60) ? "Recuperaçao" : "Reprovado";
+		System.out.println(saidaResultado + " " + notaMedia + " " + "Aluno: " + " " + name);
 	}
-		
+
 }
