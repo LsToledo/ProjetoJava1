8ee800890ff7e2741dcd78683ccc6ea4e61b6791
LsToledo <luissoaresof.la@gmail.com>
2022-12-30 06:03:29 -0300

    Criando caixa de entrada e transformando string em double.

--- PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
+++ PrimeiroPrograma/src/curso/java/executavel/PrimeiraClasseJava.java
@@ -1,21 +1,19 @@
 package curso.java.executavel;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		int numero2=0;
-		int numero3=0;
-	for (int numero = 0; numero <= 10; numero ++) {
-		System.out.println("O numero é: "+numero);
-	}
-	while(numero2 <=15) {
-		numero2++;
-		System.out.println("O numero é: "+ numero2);
-	}
-	do {
-		numero3++;
-		System.out.println("O numero é: " + numero3);
-	}while(numero3 <20);
+		
+		
+		String idade = JOptionPane.showInputDialog("Informe a sua idade");
+		String moto = JOptionPane.showInputDialog("Informe a Sua moto");
+		double transorfmando = Double.parseDouble(moto);
+		double transorfmando2 = Double.parseDouble(idade);
+
+		double soma = (transorfmando + transorfmando2);
+		System.out.println("Sua idade é :" + soma);
 }
 }
 
